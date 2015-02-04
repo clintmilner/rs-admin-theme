bbe7bad62a94fc322d14a2b2e4637aca919fb92a
Clint Milner <clinton.milner@gmail.com>
2015-02-04 11:11:56 +0000

    more dependency moving

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var gulp = require( 'gulp' ),
     uglify = require( 'gulp-uglify' ),
     concat = require( 'gulp-concat' );
 
-var masterLess, lessSources, jsSources, jsLibSources, htmlSources;
+var masterLess, lessSources, jsSources, jsLibSources, cssLibSources, webfontSources, htmlSources;
 
 masterLess = 'components/less/rs-theme.less';
 lessSources = [ 'components/less/*.less', 'components/less/mixins/*.less' ];
+cssLibSources = [ 'components/css/lib/*.css' ];
+webfontSources = [ 'components/css/fonts/*' ];
 jsSources = [ 'components/js/*.js' ];
-jsLibSources = [ 'components/js/libs/*.js' ];
+jsLibSources = [ 'components/js/lib/*.js' ];
 htmlSources = [ 'pages/*.html' ];
 
 
@@ -18,7 +20,14 @@ gulp.task( 'less', function(){
         .pipe( less() )
         .pipe( gulp.dest( 'build/css' ) );
 });
-
+gulp.task( 'cssLibs', function(){
+    gulp.src( cssLibSources )
+        .pipe( gulp.dest( 'build/css/lib' ) );
+});
+gulp.task( 'webfonts', function(){
+    gulp.src( webfontSources )
+        .pipe( gulp.dest( 'build/css/fonts' ) );
+});
 gulp.task( 'js', function(){
     gulp.src( jsSources )
         .pipe( concat( 'rs-scripts.js' ) )
@@ -26,19 +35,18 @@ gulp.task( 'js', function(){
 });
 gulp.task( 'jsLibs', function(){
    gulp.src( jsLibSources )
-       .pipe( uglify() )
        .pipe( gulp.dest( 'build/js/lib' ) );
 });
-
 gulp.task( 'html', function(){
    return gulp.src( htmlSources );
 });
-
 gulp.task( 'watch', function(){
     gulp.watch( lessSources, [ 'less' ] );
     gulp.watch( jsSources, [ 'js' ] );
     gulp.watch( jsLibSources, [ 'jsLibs' ] );
+    gulp.watch( cssLibSources, [ 'cssLibs' ] );
+    gulp.watch( webfontSources, [ 'webfonts' ] );
     gulp.watch( htmlSources, [ 'html' ] );
 });
 
-gulp.task( 'default', [ 'less', 'js', 'jsLibs', 'html', 'watch' ] );
+gulp.task( 'default', [ 'less', 'cssLibs', 'webfonts', 'js', 'jsLibs', 'html', 'watch' ] );
